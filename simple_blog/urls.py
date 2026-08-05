33aace6830d000de313235e2ec7850fe5f8d74c7
Abdulaziz <abdu111222laziz@gmail.com>
2026-08-05 19:23:38 +0500

    Fix media files routing for production

--- simple_blog/urls.py
+++ simple_blog/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 # urlpatterns = [
 
@@ -21,3 +24,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
